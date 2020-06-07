c0b95a4187d2ba5d288090d8a94c3b169edace2d
st234pa <stephanie.yoon123@gmail.com>
2020-06-07 16:24:24 -0400

    created Recipients component file, create states for App

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
+  const [recipients, setRecipients] = React.useState<string[]>([]);
+  const [cc, setCc] = React.useState<string[]>([]);
+  const [bcc, setBcc] = React.useState<string[]>([]);
+  const [content, setContent] = React.useState<string>('');
+  const [mailToUrl, setMailToUrl] = React.useState<string>('');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Hello</p>
     </div>
   );
 }
-
-export default App;
