49fe42ed470f83fdac90ade91ca8f906f45e9940
st234pa <stephanie.yoon123@gmail.com>
2020-06-08 03:32:23 -0400

    add error message tocontent component

--- app/src/components/Content.tsx
+++ app/src/components/Content.tsx
@@ -20,73 +20,49 @@ export default function Content(props: ContentProps) {
   if (props.isHidden) {
     return;
   }
+  let formControl: any;
   if (props.isLongText) {
-    return (
-      <Form.Group>
-        <Form.Label className="input-label">{props.label}</Form.Label>&nbsp;
-      </Form.Group>
+    formControl = (
+      <Form.Control
+        as="textarea"
+        rows={10}
+        placeholder={props.placeholder}
+        onChange={handleChange}
+        value={props.value}
+      />
     );
   } else {
-    return (
-      <Form.Group>
-        <Form.Label className="input-label">{props.label}</Form.Label>&nbsp;
-      </Form.Group>
+    formControl = (
+      <Form.Control
+        className={'input ' + (props.errorMessage && ' is-invalid')}
+        placeholder={props.placeholder}
+        value={props.value}
+        onChange={handleChange}
+      />
     );
   }
+  let formText: any;
+  if (props.helperText) {
+    formText = <Form.Text className="text-muted">{props.helperText}</Form.Text>;
+  } else {
+    formText = null;
+  }
+  let formFeedback: any;
+  if (props.errorMessage) {
+    formFeedback = (
+      <Form.Control.Feedback type="invalid">
+        {props.errorMessage}
+      </Form.Control.Feedback>
+    );
+  } else {
+    formFeedback = null;
+  }
+  return (
+    <Form.Group>
+      <Form.Label className="input-label">{props.label}</Form.Label>&nbsp;
+      {formControl}
+      {formText}
+      {formFeedback}
+    </Form.Group>
+  );
 }
-
-// let formText: any;
-// if (props.helperText) {
-//   formText = <Form.Text className="text-muted">{props.helperText}</Form.Text>;
-// } else {
-//   formText = <div></div>;
-// }
-// let formFeedback: any;
-// if (props.errorMessage) {
-//   formFeedback = (
-//     <Form.Control.Feedback type="invalid">
-//       {props.errorMessage}
-//     </Form.Control.Feedback>
-//   );
-// } else {
-//   formFeedback = <div></div>;
-// }
-// let formControl: any;
-// if (props.isLongText) {
-//   formControl = (
-//     <Form.Control
-//       as="textarea"
-//       rows={10}
-//       placeholder={props.placeholder}
-//       onChange={handleChange}
-//       value={props.value}
-//     />
-//   );
-// } else {
-//   formControl = (
-//     <Form.Control
-//       className="input"
-//       placeholder={props.placeholder}
-//       value={props.value}
-//       onChange={handleChange}
-//     >
-//       {/* <Form.Text className="text-muted">
-//           Press enter, space, or comma to add to recipients list.
-//         </Form.Text> */}
-
-//       {/* {props.errorMessage && (
-//           <Form.Control.Feedback type="invalid">
-//             {props.errorMessage}
-//           </Form.Control.Feedback>
-//         )} */}
-//     </Form.Control>
-//   );
-// }
-
-// return (
-//   <Form.Group>
-//     <Form.Label className="input-label">{props.label}</Form.Label>&nbsp;
-//     {formControl}
-//     {formFeedback}
-//   </Form.Group>
-// );
