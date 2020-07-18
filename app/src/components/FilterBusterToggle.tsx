4dbdc4ef74f52631b751930fdb3d19fe5c799e17
st234pa <stephanie.yoon123@gmail.com>
2020-07-18 15:50:42 -0400

    edit language describing filter buster

--- app/src/components/FilterBusterToggle.tsx
+++ app/src/components/FilterBusterToggle.tsx
@@ -24,7 +24,7 @@ function FilterBusterToggle(props: FilterBusterToggleProps) {
             <Form.Text className="text-muted">
               When Filter Buster is on, the generated link will direct users to
               Filter Buster, which will add randomized characters to each word
-              in the subject and body, thereby avoiding email filters!
+              in the subject and body, thereby bypassing email filters!
             </Form.Text>
           </Form.Group>
         </Form>
